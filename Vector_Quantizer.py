de77cd924a4348d31924db866063cf5f3819306f
ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>
2025-04-07 21:30:23 +0300

    Vector Quantizer added

--- Vector_Quantizer.py
+++ Vector_Quantizer.py
@@ -10,37 +10,29 @@ class VectorQuantizer(nn.Module):
         self.embedding_dim = embedding_dim
         self.commitment_cost = commitment_cost
         
-        # Initialize embeddings
         self.embedding = nn.Embedding(self.num_embeddings, self.embedding_dim)
         self.embedding.weight.data.uniform_(-1/self.num_embeddings, 1/self.num_embeddings)
         
     def forward(self, inputs):
-        # Convert inputs from BCHW -> BHWC
         inputs = inputs.permute(0, 2, 3, 1).contiguous()
         input_shape = inputs.shape
         
-        # Flatten input
         flat_input = inputs.view(-1, self.embedding_dim)
         
-        # Calculate distances
         distances = (torch.sum(flat_input**2, dim=1, keepdim=True) 
                     + torch.sum(self.embedding.weight**2, dim=1)
                     - 2 * torch.matmul(flat_input, self.embedding.weight.t()))
         
-        # Encoding
         encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
         encodings = torch.zeros(encoding_indices.shape[0], self.num_embeddings, device=inputs.device)
         encodings.scatter_(1, encoding_indices, 1)
         
-        # Quantize and unflatten
         quantized = torch.matmul(encodings, self.embedding.weight).view(input_shape)
         
-        # Loss
         e_latent_loss = F.mse_loss(quantized.detach(), inputs)
         q_latent_loss = F.mse_loss(quantized, inputs.detach())
         loss = q_latent_loss + self.commitment_cost * e_latent_loss
         
-        quantized = inputs + (quantized - inputs).detach()  # Straight-through estimator
+        quantized = inputs + (quantized - inputs).detach()
         
-        # Convert quantized from BHWC -> BCHW
         return quantized.permute(0, 3, 1, 2).contiguous(), loss, encoding_indices
